e34d3e114716dd94b0d290b33d7d3f2b2bfb0ad4
Tevin Stowers <tstowers@Tevins-MacBook-Pro-2.local>
2020-04-04 08:39:01 -0400

    stage 1

---
+++ companies.js
@@ -0,0 +1,12 @@
+function myFunction() {
+ 
+   let url =  prompt("Copy and paste URL of company logo:"); 
+   window.open(url); 
+   
+ 
+
+  }
+
+  // window.location = url; 
+  // window.open(url);
+  // document.getElementById("addedURL").innerHTML = `${url}`; 
