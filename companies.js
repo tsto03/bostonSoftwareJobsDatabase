04f699b154e07fead5a9a549701ce04e96f4be7d
Tevin Stowers <tstowers@Tevins-MacBook-Pro-2.local>
2020-04-05 18:40:00 -0400

    updated html and js

--- companies.js
+++ companies.js
@@ -1,12 +1,13 @@
 function myFunction() {
  
    let url =  prompt("Copy and paste URL of company logo:"); 
-   window.open(url); 
+   document.getElementById("addedURL").innerHTML = `${url}`; 
    
  
 
   }
 
+//   window.open(url); 
   // window.location = url; 
   // window.open(url);
-  // document.getElementById("addedURL").innerHTML = `${url}`; 
+  
